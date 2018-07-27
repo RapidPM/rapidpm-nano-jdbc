92be0f73eb0003757dadeba0fb5bb11c43cd88e0
Sven Ruppert <sven.ruppert@gmail.com>
2018-07-27 16:16:21 +0200

    init

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module org.rapidpm.nano.jdbc {
   requires rapidpm.dependencies.core;
   requires rapidpm.dependencies.core.logger;
-  requires rapidpm.functional.reactive;
+  requires rapidpm.functional.reactive ;
   requires jsr305;
   requires java.sql;
   requires com.zaxxer.hikari;
